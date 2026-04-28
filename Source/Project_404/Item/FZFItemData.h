0566239e1df2b230db8b9df9757033672b5a78d9
Kim youngwoo <keiprogramo@gmail.com>
2026-04-28 17:34:01 +0900

    [feat] : 주석 추가.
    추가 주석 기입.

--- Source/Project_404/Item/FZFItemData.h
+++ Source/Project_404/Item/FZFItemData.h
@@ -29,6 +29,8 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "World")
     FVector MeshScale = FVector(1.0f, 1.0f, 1.0f);
 
+    // 메쉬 사이즈를 Trigger Collision의 사이즈에 맞게 자동으로 조정해주는 변수.
+    // 사용 이유 : 애셋들의 모든 사이즈를 Trigger Collision의 사이즈에 맞게 일일이 수동으로 해주기보다는 자동으로 맞춰주도록 해주기 위해.
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "World")
     bool bAutoFitMeshToTrigger = true;
 
