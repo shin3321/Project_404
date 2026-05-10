d3bb869f43497cba7d8aca1842f24aa621f65f28
tuktarr <68638065+tuktarr@users.noreply.github.com>
2026-05-11 00:01:16 +0900

    [feat] Monster에 MonsterSet 추가
    - MonsterSet 추가
    - 인터페이스 로직 GAS로 변경할 것 주석에 표기

---
+++ Source/Project_404/GAS/Attributes/FZFMonsterSet.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Attributes/FZFAttributeSet.h"
+#include "FZFMonsterSet.generated.h"
+
+UCLASS()
+class PROJECT_404_API UFZFMonsterSet : public UFZFAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UFZFMonsterSet();
+
+	// 공격 사거리 (Attack Range)
+	ATTRIBUTE_ACCESSORS(UFZFMonsterSet, AttackRange);
+	// 탐지 거리 (Detection Range)
+	ATTRIBUTE_ACCESSORS(UFZFMonsterSet, DetectRange);
+	// 회전 속도 (Turn Speed)
+	ATTRIBUTE_ACCESSORS(UFZFMonsterSet, TurnSpeed);
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData AttackRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData DetectRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData TurnSpeed;
+};
