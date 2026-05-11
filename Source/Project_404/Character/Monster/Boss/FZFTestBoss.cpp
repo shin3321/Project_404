9ab73bc8d89b833157bdd8e7309f533042f8ea12
sine <qkrtlsgp0996@gmail.com>
2026-05-11 16:07:32 +0900

    [feat] 보스 레벨 매니저 생성, 콜리전, 트레이스 채널 추가

---
+++ Source/Project_404/Character/Monster/Boss/FZFTestBoss.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Monster/Boss/FZFTestBoss.h"
+
+// Sets default values
+AFZFTestBoss::AFZFTestBoss()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFZFTestBoss::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFZFTestBoss::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFZFTestBoss::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AFZFTestBoss::CheckBossPhase()
+{
+	EBossPhase NextBossPhase = EBossPhase::Phase1;
+	if (NextBossPhase != CurrentBossPhase)
+	{
+		CurrentBossPhase = NextBossPhase;
+		OnBossPhaseChanged.Broadcast(CurrentBossPhase);
+	}
+}
+
