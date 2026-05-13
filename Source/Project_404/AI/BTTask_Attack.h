b677dff2747abd6dd1dff04ea34b0f554e9d4476
dalssak <dalssagi00@gmail.com>
2026-05-13 14:55:56 +0900

    [feat] ChangeMoveSpeed Task 제작 중

--- Source/Project_404/AI/BTTask_Attack.h
+++ Source/Project_404/AI/BTTask_Attack.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -18,7 +18,5 @@ public:
 	UBTTask_Attack();
 
 protected:
-	virtual EBTNodeResult::Type ExecuteTask(
-		UBehaviorTreeComponent& OwnerComp,
-		uint8* NodeMemory) override;
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp,uint8* NodeMemory) override;
 };
---
+++ Source/Project_404/AI/BTTask_ChangeMoveSpeed.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask_ChangeMoveSpeed.h"
+
+UBTTask_ChangeMoveSpeed::UBTTask_ChangeMoveSpeed()
+{
+	NodeName = TEXT("ChangeMoveSpeed");
+
+}
+
+EBTNodeResult::Type UBTTask_ChangeMoveSpeed::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+
+
+	return EBTNodeResult::Type();
+}
