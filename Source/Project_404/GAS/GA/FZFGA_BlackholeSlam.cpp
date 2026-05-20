cd6b32631a6f4a4018218066e0ba22d5c872bbaa
tuktarr <68638065+tuktarr@users.noreply.github.com>
2026-05-20 16:38:17 +0900

    [feat] Monster (Head) 추가 및  GA, GE 생성
    - Asset 폴더에 Animation 추가 (SC_Head)
    - GA, GE 생성 (끌어당기기, 장판형 데미지 주기)

---
+++ Source/Project_404/GAS/GA/FZFGA_BlackholeSlam.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GA/FZFGA_BlackholeSlam.h"
+
+UFZFGA_BlackholeSlam::UFZFGA_BlackholeSlam()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UFZFGA_BlackholeSlam::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+}
+
+void UFZFGA_BlackholeSlam::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+}
+
+void UFZFGA_BlackholeSlam::OnPullFinished()
+{
+}
+
+void UFZFGA_BlackholeSlam::OnSlamHitEventReceived(FGameplayEventData Payload)
+{
+}
+
+void UFZFGA_BlackholeSlam::OnMontageCompleted()
+{
+}
---
+++ Source/Project_404/GAS/GA/FZFGA_BlackholeSlam.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "FZFGA_BlackholeSlam.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_404_API UFZFGA_BlackholeSlam : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFZFGA_BlackholeSlam();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+
+	// Pulling 종료 후 점프 시작
+	UFUNCTION()
+	void OnPullFinished();
+
+	// 착지 시, 데미지 이벤트 발생 
+	UFUNCTION()
+	void OnSlamHitEventReceived(FGameplayEventData Payload);
+
+	// 몽타주 재생 종료
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	// 스킬 설정 변수들
+	UPROPERTY(EditDefaultsOnly, Category = "Blackhole")
+	float PullDuration = 2.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Blackhole")
+	float SlamRadius = 500.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Blackhole")
+	TSubclassOf<class UGameplayEffect> SlamDamageEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Blackhole")
+	class UAnimMontage* BlackholeSlamMontage;
+
+private:
+	FTimerHandle PullTimerHandle;
+};
