e5bf5c86fd72aafc3ebb7a280cdb96daff659584
sine <qkrtlsgp0996@gmail.com>
2026-05-26 17:25:03 +0900

    [fix] 스폰 매니저 수정

--- Source/Project_404/Manager/FZFSpawnManager.cpp
+++ Source/Project_404/Manager/FZFSpawnManager.cpp
@@ -106,7 +106,7 @@ void AFZFSpawnManager::BeginPlay()
 	// 4. 아이템 스폰 실행
 	if (ItemSlots.Num() != 0 && ItemPool.Num() != 0)
 	{
-		int32 MaxItemCount = FMath::Max(ItemSlots.Num(), ItemPool.Num());
+		int32 MaxItemCount = FMath::Min(ItemSlots.Num(), ItemPool.Num());
 		for (int32 i = 0; i < MaxItemCount; ++i)
 		{
 			AActor* Slot = ItemSlots[i];
@@ -125,8 +125,9 @@ void AFZFSpawnManager::BeginPlay()
 
 
 	// 5. 몬스터 스폰 실행
-	if (MonsterPool.Num() != 0)
+	if (MonsterSlots.Num() != 0 && MonsterPool.Num() != 0)
 	{
+		int32 MaxItemCount = FMath::Min(MonsterSlots.Num(), MonsterPool.Num());
 		for (int32 i = 0; i < MonsterPool.Num(); ++i)
 		{
 			AActor* Slot = MonsterSlots[i];
