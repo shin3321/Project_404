7b766e97524977b96426b4f1230395836082fa7c
tuktarr <68638065+tuktarr@users.noreply.github.com>
2026-05-27 09:13:22 +0900

    [fix] Boss Level Robot 수정
    Robot 클래스 추가 및 배치

---
+++ Source/Project_404/Utils/FZFRobot.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Utils/FZFRobot.h"
+
+// Sets default values
+AFZFRobot::AFZFRobot()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AFZFRobot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Project_404/Utils/FZFRobot.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FZFRobot.generated.h"
+
+UCLASS()
+class PROJECT_404_API AFZFRobot : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AFZFRobot();
+
+protected:
+	virtual void BeginPlay() override;
+
+};
