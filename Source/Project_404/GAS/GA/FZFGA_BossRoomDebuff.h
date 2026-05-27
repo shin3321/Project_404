4db313ad736d16c267b80cc1f8e490a1edddead2
tuktarr <68638065+tuktarr@users.noreply.github.com>
2026-05-27 12:25:51 +0900

    [feat] Player에 InBossRoomDebuff Ability 추가

---
+++ Source/Project_404/GAS/GA/FZFGA_BossRoomDebuff.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "FZFGA_BossRoomDebuff.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_404_API UFZFGA_BossRoomDebuff : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFZFGA_BossRoomDebuff();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	// 주기적으로 체력을 감소 시킬 GameplayEffect 클래스
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debuff")
+	TSubclassOf<class UGameplayEffect> DOTEffectClass;
+
+	// 생성된 효과를 나중에 해제하기 위해 저장해둘 핸들
+	FActiveGameplayEffectHandle ActiveDOTEffectHandle;
+};
--- Source/Project_404/GameplayTag/FZFGameplayTags.h
+++ Source/Project_404/GameplayTag/FZFGameplayTags.h
@@ -56,6 +56,7 @@ namespace FZFGameplayTags
 	// 디버프 태그
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Debuff_Stun);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Debuff_Fire);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Debuff_InBossRoom);
 
 	// 어트리뷰트 데이터 초기화용 태그
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Stat_MaxHp);
